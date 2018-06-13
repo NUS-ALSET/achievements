c9d8584b6ae390091f921b9f1f30b1aacfa08e20
Brahms <yangkaiyuan@u.nus.edu>
2018-06-14 01:54:18 +0800

    config.js detect env.NODE_ENV for production for Firebase

--- src/achievementsApp/config.js
+++ src/achievementsApp/config.js
@@ -11,14 +11,26 @@ export const APP_SETTING = {
 };
 
 // Initialize Firebase
-const config = {
-  apiKey: "AIzaSyA232bBlzWT0fl3ST_KVC3Aay41yTMz5vM",
-  authDomain: "achievements-dev.firebaseapp.com",
-  databaseURL: "https://achievements-dev.firebaseio.com",
-  projectId: "achievements-dev",
-  storageBucket: "achievements-dev.appspot.com",
-  messagingSenderId: "479020625755"
-};
+// start script's process.env.NODE_ENV = 'development';
+// build script's process.env.NODE_ENV = 'production';
+// use different firebase database for different script
+const config = {};
+if (process.env.NODE_ENV === 'development') {
+  config.apiKey = "AIzaSyA232bBlzWT0fl3ST_KVC3Aay41yTMz5vM";
+  config.authDomain = "achievements-dev.firebaseapp.com";
+  config.databaseURL = "https://achievements-dev.firebaseio.com";
+  config.projectId = "achievements-dev";
+  config.storageBucket = "achievements-dev.appspot.com";
+  config.messagingSenderId = "479020625755";
+}
+if (process.env.NODE_ENV === 'production') {
+  config.apiKey = "AIzaSyDQuGo8wKcxLbLZo56aYWLTdP2qrbMamYQ";
+  config.authDomain = "achievements-prod.firebaseapp.com";
+  config.databaseURL = "https://achievements-prod.firebaseio.com";
+  config.projectId = "achievements-prod";
+  config.storageBucket = "achievements-prod.appspot.com";
+  config.messagingSenderId = "829016923358";
+}
 firebase.initializeApp(config);
 
 export const authProvider = new firebase.auth.GoogleAuthProvider();
