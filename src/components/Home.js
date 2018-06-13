b859819f9a80a07f72cb164d1032548ff5ab83c0
Brahms <yangkaiyuan@u.nus.edu>
2018-06-13 11:29:45 +0800

    separate route /homev2 to test kyGUI homeview

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,10 @@
 import React from "react";
-import RecommendationListCardPy from "../containers/HomeView/RecommendationListCardPy";
-import RecommendationListCardYo from "../containers/HomeView/RecommendationListCardYo";
 
 export class Home extends React.PureComponent {
   render() {
     return (
       <div>
-        <RecommendationListCardPy />
-        <br />
-        <RecommendationListCardYo />
+        Under Development
       </div>
     );
   }
--- src/containers/AppFrame/AppFrame.js
+++ src/containers/AppFrame/AppFrame.js
@@ -33,6 +33,9 @@ import Admin from "../Admin/Admin";
 import Problem from "../Problem/Problem";
 import Path from "../Path/Path";
 
+// HomeV2 to test the kyGUI for Home Recommendation
+import HomeV2 from "../HomeView/HomeV2";
+
 const styles = theme => ({
   "@global": {
     html: {
@@ -208,6 +211,7 @@ class AppFrame extends React.Component {
             />
             <main className={classes.content}>
               <Route component={Home} exact path="(/|/home)" />
+              <Route component={HomeV2} exact path="(/homev2)" />
               <Route component={Admin} exact path="/admin" />
               <Route component={Courses} exact path="/courses" />
               <Route component={Assignments} exact path="/courses/:courseId" />
