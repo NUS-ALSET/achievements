eb27de0e2c9ffc67c19a29b9c43d353087fd4dfb
Brahms <yangkaiyuan@u.nus.edu>
2018-06-17 19:44:23 +0800

    model data code separation after Theodor's for kyHomev2

--- src/containers/HomeView/SampleCard.jsx
+++ src/containers/HomeView/SampleCard.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+
+import { Link } from "react-router-dom";
+
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+
+/*
+ * the data code design is modeled after
+ * Theodor Shaytanov's PathCard container module
+ */
 
 const styles = {
   card: {
@@ -25,38 +32,50 @@ const styles = {
   },
 };
 
-class SampleCard extends React.Component {
+class SampleCard extends React.PureComponent {
+  static propTypes = {
+    // key is not a prop
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    path: PropTypes.string,
+    problem: PropTypes.string
+  };
+
   render() {
-    const { classes } = this.props;
+    // get the data from the dummy Redux State
+    // props.path is the owner value
+    // props.problem is the key value of the owner
+    const { title, description, path, problem } = this.props;
+
     return (
-      <div>
-        <Card className={classes.card}>
-          <CardMedia
-            className={classes.media}
-            image="https://www.python.org/static/community_logos/python-logo-master-v3-TM-flattened.png"
-            title="Python Language"
-          />
-          <CardContent className={classes.content}>
-            <Typography variant="headline" component="p">
-              If Then
-            </Typography>
-            <Typography component="p">
-              Learn about Python conditional statements to write useful programs. The simplest form is the if statement, and there are more like while and for...
-            </Typography>
-          </CardContent>
-          <CardActions>
+      <Card style={styles.card}>
+        <CardMedia
+          style={styles.media}
+          image="https://www.python.org/static/community_logos/python-logo-master-v3-TM-flattened.png"
+          title="Python Language"
+        />
+        <CardContent style={styles.content}>
+          <Typography variant="headline" component="p">
+            {title}
+          </Typography>
+          <Typography component="p">
+            {description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Link
+            to={`/paths/${path}/problems/${problem}`}
+            style={{ textDecoration: 'none' }}
+          >
             <Button size="small" color="primary">
               Learn More
             </Button>
-          </CardActions>
-        </Card>
-      </div>
+          </Link>
+        </CardActions>
+      </Card>
     );
   }
 }
 
-SampleCard.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
 
-export default withStyles(styles)(SampleCard);
+export default SampleCard;
--- src/containers/HomeView/SampleCarousel.jsx
+++ src/containers/HomeView/SampleCarousel.jsx
@@ -1,26 +1,49 @@
 import React from 'react';
+import PropTypes from "prop-types";
+
 import './NukaCarouselStyle.css';
 /* the Carousel is from nuka-carousel
  * by Ken Wheeler
  * more @ http://kenwheeler.github.io/nuka-carousel/ 
  */
 import Carousel from 'nuka-carousel';
+
+/*
+ * the data code design is modeled after
+ * Theodor Shaytanov's PathCard container module
+ */
 import SampleCard from './SampleCard';
 
-class SampleCarousel extends React.Component {
+
+// TODO: SampleCarousel and SampleCard will be stateless PureComponents
+// TODO: use the render*Controls for Nuka-Carousel to disable arrow 
+
+class SampleCarousel extends React.PureComponent {
+  static propTypes = {
+    dataList: PropTypes.array,
+    youtubeRecom: PropTypes.bool,
+  };
+
   render() {
+    // retrieve the dummyData
+    const { dataList } = this.props;
+    console.log("??????", dataList);
+
     return (
       <Carousel
         slidesToShow={4}
         cellSpacing={10}
         className="customizeCarousel"
       >
-        <SampleCard />
-        <SampleCard />
-        <SampleCard />
-        <SampleCard />
-        <SampleCard />
-        <SampleCard />
+        {dataList.map( (item, index) => (
+          <SampleCard
+            key={index}
+            title={item.name}
+            description={`${item.type} Python problem`}
+            path={item.owner}
+            problem={item.actualProblem}
+          />
+        ))}
       </Carousel>
     );
   }
