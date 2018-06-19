b147d2a16928e7adcb7ed499b50dda576642701e
Theodor Shaytanov <theodor.shaytanov@gmail.com>
2018-06-19 11:27:46 +0300

    Add common http fetcher to firebase functions

---
+++ functions/src/utils/http.js
@@ -0,0 +1,73 @@
+const uuid = require("uuid");
+const admin = require("firebase-admin");
+
+class HTTPUtil {
+  /**
+   * Call HTTP resource
+   *
+   * @param {String} url HTTP url for request
+   * @param {String} [method=get] HTTP method
+   * @param {any} [data] data for request
+   * @returns {Promise<any>} promise with request processing result
+   */
+  call(url, method, data) {
+    const taskKey = uuid();
+
+    if (!url) {
+      throw new Error("Missing required `url` parameter");
+    }
+
+    return new Promise((resolve, reject) => {
+      try {
+        admin
+          .database()
+          .ref("/outgoingRequestsQueue/tasks")
+          .push({
+            url,
+            method: method || "get",
+            data: data || null,
+            taskKey
+          })
+          .then(() => {
+            const answerRef = admin
+              .database()
+              .ref(`/outgoingRequestsQueue/answers/${taskKey}`);
+            let skipFirst;
+            return answerRef.on("value", response => {
+              if (!skipFirst) {
+                return (skipFirst = true);
+              }
+              answerRef.off();
+              answerRef.remove();
+              resolve(response.val());
+            });
+          });
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+  /**
+   * Call HTTP resource with GET method
+   *
+   * @param {String} url HTTP url for request
+   * @param {any} [data] data for request
+   * @returns {Promise<any>} promise with request processing result
+   */
+  get(url, data) {
+    return this.call(url, "get", data);
+  }
+  /**
+   * Call HTTP resource with POST method
+   *
+   * @param {String} url HTTP url for request
+   * @param {any} [data] data for request
+   * @returns {Promise<any>} promise with request processing result
+   */
+  post(url, data) {
+    return this.call(url, "post", data);
+  }
+}
+
+exports.HTTPUtil = HTTPUtil;
+exports.httpUtil = new HTTPUtil();
