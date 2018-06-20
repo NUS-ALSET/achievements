ea21ea9e75ba4545d46a20ee7a051934ac155ef5
Yan Rong <35908515+yanrongabc@users.noreply.github.com>
2018-06-20 09:39:13 +0800

    Create index.js

---
+++ functions/index.js
@@ -0,0 +1,88 @@
+/* eslint-disable no-magic-numbers */
+
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+const checkToken = require("./src/utils/checkToken");
+
+const api = require("./src/api");
+const ltiLogin = require("./src/ltiLogin");
+const profileTriggers = require("./src/updateProfile");
+const jupyterTrigger = require("./src/executeJupyterSolution");
+const downloadEvents = require("./src/downloadEvents");
+const solutionTriggers = require("./src/updateSolutionVisibility");
+
+const profilesRefreshApproach =
+  (functions.config().profiles &&
+    functions.config().profiles["refresh-approach"]) ||
+  "none";
+const ERROR_500 = 500;
+
+admin.initializeApp();
+
+exports.handleNewProblemSolution =
+  ["trigger", "both"].includes(profilesRefreshApproach) &&
+  functions.database
+    .ref("/jupyterSolutionsQueue/tasks/{requestId}")
+    .onWrite(change => {
+      const data = change.after.val();
+      jupyterTrigger.handler(data, data.taskKey, data.owner);
+    });
+
+exports.handleProblemSolutionQueue = functions.https.onRequest((req, res) => {
+  return checkToken(req)
+    .then(() => {
+      exports.queueHandler();
+      res.send("Done");
+    })
+    .catch(err => res.status(err.code || ERROR_500).send(err.message));
+});
+
+exports.handleNewSolution = functions.database
+  .ref("/solutions/{courseId}/{studentId}/{assignmentId}")
+  .onWrite((change, context) => {
+    const { courseId, studentId, assignmentId } = context.params;
+
+    solutionTriggers.handler(
+      change.after.val(),
+      courseId,
+      studentId,
+      assignmentId
+    );
+  });
+
+exports.handleProfileRefreshRequest =
+  ["trigger", "both"].includes(profilesRefreshApproach) &&
+  functions.database
+    .ref("/updateProfileQueue/tasks/{requestId}")
+    .onCreate((snap, context) =>
+      new Promise(resolve => profileTriggers.handler(snap.val(), resolve)).then(
+        () =>
+          admin
+            .database()
+            .ref(`/updateProfileQueue/tasks/${context.params.requestId}`)
+            .remove()
+      )
+    );
+exports.handleProfileQueue = functions.https.onRequest((req, res) => {
+  return checkToken(req)
+    .then(() => {
+      exports.queueHandler();
+      res.send("Done");
+    })
+    .catch(err => res.status(err.code || ERROR_500).send(err.message));
+});
+
+exports.api = functions.https.onRequest((req, res) => {
+  let { token, data } = req.query;
+
+  api.handler(token, data).then(output => res.send(output));
+});
+
+exports.ltiLogin = functions.https.onRequest(ltiLogin.handler);
+
+exports.downloadEvents = downloadEvents.httpTrigger;
+
+exports.yrtest = functions.https.onRequest((req, res) => {
+  res.status(200).send("YR TEST PASSED :D");
+});
