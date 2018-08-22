6f50a5437204b19bd0a389b0f5fddeb7e881f5ec
FeynmanDNA-KYY <tanuki@debian-tanuki.chaosknight>
2018-08-23 00:22:03 +0800

    TODOs and comments for HomeV2 with borrowed ideas from HomeV3

--- src/containers/HomeV2/RecommendationListCard.jsx
+++ src/containers/HomeV2/RecommendationListCard.jsx
@@ -4,9 +4,22 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Avatar from '@material-ui/core/Avatar';
-// amber color python, red for youtube
+
+// TODO: types of Activities:
+// YouTube, Jupyter, Jest, Colab, Code Combat, Game
+// Avatar with icons for certain Activity types
+import PlayArrow from '@material-ui/icons/PlayArrow'; // YouTube
+// import Adb from '@material-ui/icons/Adb'; // Game
+
+// TODO: amber color python (Jupyter and Colab), red for youtube
 import amber from '@material-ui/core/colors/amber';
 import red from '@material-ui/core/colors/red';
+// Jest:
+// import blue from '@material-ui/core/colors/blue';
+// CodeCombat:
+// import lime from '@material-ui/core/colors/lime';
+// Game:
+// import indigo from '@material-ui/core/colors/indigo';
 
 import SampleCarousel from './SampleCarousel';
 
@@ -30,7 +43,7 @@ const styles = {
   },
 };
 
-class RecommendationListCardPy extends React.PureComponent {
+class RecommendationListCard extends React.PureComponent {
   static propTypes = {
     RecomType: PropTypes.string.isRequired,
     dummyData: PropTypes.array,
@@ -41,50 +54,58 @@ class RecommendationListCardPy extends React.PureComponent {
     // here temporarily use a dummy JSON with 2 pythonlists and 1 youtubelist
     const { dummyData } = this.props;
 
+    // TODO: three major categories for Avatar icons
+    // YouTube Activities use PlayArrow for Avatar Icon,
+    // Game/CodeCombat use Adb icon,
+    // Jupyter/Jest/Codelab use '</>'
+
     return (
-      <Card style={styles.card}>
-        {(this.props.RecomType === "python") &&
-          <Fragment>
-            <CardHeader
-              avatar={
-                <Avatar aria-label="Recommendation" style={styles.avatarPy}>
-                  Py
-                </Avatar>
-              }
-              title="Jupyter Notebook Activities"
-              subheader="Recommended for you"
-            />
-            <CardContent>
-              <SampleCarousel
-                youtubeRecom={false}
-                dataList={dummyData}
+      <div style={{marginBottom : '24px'}}>
+      {/* either use marginBottom here or put a <br /> at parent */}
+        <Card style={styles.card}>
+          {(this.props.RecomType === "python") &&
+            <Fragment>
+              <CardHeader
+                avatar={
+                  <Avatar aria-label="Recommendation" style={styles.avatarPy}>
+                    {"</>"}
+                  </Avatar>
+                }
+                title="Jupyter Notebook Activities"
+                subheader="Recommended for you"
               />
-            </CardContent>
-          </Fragment>
-        }
-        {(this.props.RecomType === "youtube") &&
-          <Fragment>
-            <CardHeader
-              avatar={
-                <Avatar aria-label="Recommendation" style={styles.avatarYo}>
-                  Y
-                </Avatar>
-              }
-              title="YouTube Video Activities"
-              subheader="Recommended for you"
-            />
-            <CardContent>
-              <SampleCarousel
-                youtubeRecom={true}
-                dataList={dummyData}
+              <CardContent>
+                <SampleCarousel
+                  youtubeRecom={false}
+                  dataList={dummyData}
+                />
+              </CardContent>
+            </Fragment>
+          }
+          {(this.props.RecomType === "youtube") &&
+            <Fragment>
+              <CardHeader
+                avatar={
+                  <Avatar aria-label="Recommendation" style={styles.avatarYo}>
+                    <PlayArrow />
+                  </Avatar>
+                }
+                title="YouTube Video Activities"
+                subheader="Recommended for you"
               />
-            </CardContent>
-          </Fragment>
-        }
-      </Card>
+              <CardContent>
+                <SampleCarousel
+                  youtubeRecom={true}
+                  dataList={dummyData}
+                />
+              </CardContent>
+            </Fragment>
+          }
+          {/* other types of Activities with different RecomType etc */}
+        </Card>
+      </div>
     );
   }
 }
 
-
-export default RecommendationListCardPy;
+export default RecommendationListCard;
