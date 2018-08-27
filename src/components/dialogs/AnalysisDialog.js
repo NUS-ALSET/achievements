529d8f915510cdbdd29ef6d0799d0d1a6fbd0a78
FeynmanDNA-KYY <tanuki@debian-tanuki.chaosknight>
2018-08-27 18:20:36 +0800

    reformat AnalysisDialog code

--- src/components/dialogs/AnalysisDialog.js
+++ src/components/dialogs/AnalysisDialog.js
@@ -1,5 +1,3 @@
-
-
 import React from "react";
 
 import Button from "@material-ui/core/Button";
@@ -10,30 +8,30 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 
 
-const AnalysisDialog=(props)=>{
+const AnalysisDialog = (props) => {
   return (
     <Dialog
-          open={props.open}
-          onClose={props.handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-          maxWidth={"md"}
-        >
-          <DialogTitle id="alert-dialog-title">{"Jupter Inline Problem Analysis"}</DialogTitle>
-          <DialogContent>
-          
-              <pre style={{ color: 'black', lineHeight: '1.5', padding : '0px 20px'}}>
-                {JSON.stringify(props.skills, null, '  ')}
-              </pre>
-          
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={props.handleClose} color="primary" autoFocus>
-              Close
-            </Button>
-          </DialogActions>
-        </Dialog>
-  )
-}
+      open={props.open}
+      onClose={props.handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+      maxWidth={"md"}
+    >
+      <DialogTitle id="alert-dialog-title">
+        {"Jupter Inline Problem Analysis"}
+      </DialogTitle>
+      <DialogContent>
+        <pre style={{ color: 'black', lineHeight: '1.5', padding : '0px 20px'}}>
+          {JSON.stringify(props.skills, null, '  ')}
+        </pre>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={props.handleClose} color="primary" autoFocus>
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
 
 export default AnalysisDialog;
