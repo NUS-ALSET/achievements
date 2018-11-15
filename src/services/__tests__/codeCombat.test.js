3663cf5042f5107657e22c890062a3e354e42834
Chris Boesch <scboesch@gmail.com>
2018-11-15 15:27:20 +0800

    Adding initial tests for codeCombat.js

---
+++ src/services/__tests__/codeCombat.test.js
@@ -0,0 +1,14 @@
+import assert from "assert";
+import { CodeCombatService } from "../codeCombat";
+
+describe("courses service tests", () => {
+  /** @type {CodeCombatService} */
+  let service;
+
+  beforeEach(() => (service = new CodeCombatService()));
+
+  it("Sliggify should work", () => {
+    let result = service.sliggify("hap py");
+    expect(result).toEqual("hap-py");
+  });
+});
