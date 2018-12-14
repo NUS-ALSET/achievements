6abe58a21a437345dbd31a20b141055d6446ebc2
bensj <bensjy_bensjy@hotmail.com>
2018-12-14 11:56:51 +0800

    Added charts under ben's hidden route

--- src/containers/Debug/Ben.js
+++ src/containers/Debug/Ben.js
@@ -3,6 +3,8 @@ import { connect } from "react-redux";
 import { compose } from "redux";
 
 import { firebaseConnect } from "react-redux-firebase";
+import { ScatterChart, Scatter, LineChart, Line, BarChart, Bar, XAxis, YAxis, CartesianGrid, Legend} from 'recharts';
+
 
 class Ben extends React.PureComponent {
     // constructor(props){
@@ -13,11 +15,56 @@ class Ben extends React.PureComponent {
         return (
             <Fragment>
                 <h1>Ben's route</h1>
-                <div>This portion will be replaced by some charts</div>
+				<h2><a href="https://moduscreate.com/blog/ext-js-to-react-charting/">Reference</a></h2>
+
+				<h3> This is a line chart</h3>
+				<LineChart width={730} height={250} data={data}>
+					<CartesianGrid strokeDasharray="3 3" />
+					<XAxis dataKey="name" />
+					<YAxis />
+					<Legend />
+					<Line type="monotone" dataKey="Category1" stroke="#8884d8" />
+					<Line type="monotone" dataKey="Category2" stroke="#82ca9d" />
+				</LineChart>
+
+				<hr></hr>
+
+				<h3>This is a bar chart</h3> 
+				<BarChart width={730} height={250} data={data}>
+					<CartesianGrid strokeDasharray="3 3" />
+					<XAxis dataKey="name" />
+					<YAxis />
+					<Legend />
+					<Bar dataKey="Category1" fill="#8884d8" />
+					<Bar dataKey="Category2" fill="#82ca9d" />
+				</BarChart>
+
+				<hr></hr>
+
+				<h3>This is a scatter chart</h3> 
+				<ScatterChart width={730} height={250}>
+					<CartesianGrid strokeDasharray="3 3" />
+					<XAxis dataKey="Category1" />
+					<YAxis dataKey="Category2" />
+					<Legend />
+					<Scatter name='Category1 / Category2' data={data} fill='#8884d8'/>
+				</ScatterChart>
             </Fragment>
         );
     }
 }
+
+// This is where you store your data for your rechart charts
+const data = [
+	{name: 'A', Category1: 4000, Category2: 2400},
+	{name: 'B', Category1: 3000, Category2: 1398},
+	{name: 'C', Category1: 2000, Category2: 9800},
+	{name: 'D', Category1: 2780, Category2: 3908},
+	{name: 'E', Category1: 1890, Category2: 4800},
+	{name: 'F', Category1: 2390, Category2: 3800},
+	{name: 'G', Category1: 3490, Category2: 4300}
+];
+  
 const mapStateToProps = (state, ownProps) => ({
     uid: state.firebase.auth.uid,
     completedActivities : state.firebase.ordered.completedActivities,
