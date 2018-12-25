1edf8e9dfcec91f836657fd55e148b9a9107ac14
FeynmanDNA <26617036+FeynmanDNA@users.noreply.github.com>
2018-12-25 17:17:07 +0800

    remove not used Enzyme

--- src/components/NotificationArea.test.js
+++ src/components/NotificationArea.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import NotificationArea from "./NotificationArea";
 import IconButton from "@material-ui/core/IconButton";
-import Enzyme, { shallow, render, mount } from "enzyme";
+import { shallow, mount } from "enzyme";
 import sinon from "sinon";
 
 describe("<NotificationArea>", () => {
