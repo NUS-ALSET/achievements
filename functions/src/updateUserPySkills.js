80ee979809f2095eb06927b9426db740aeafb8c0
walkwel <developer.up90@gmail.com>
2019-04-26 16:49:03 +0530

    timestamp saved with user skills

--- functions/src/updateUserPySkills.js
+++ functions/src/updateUserPySkills.js
@@ -1,8 +1,8 @@
 const admin = require("firebase-admin");
 
 exports.handler = (newSolution, studentId, assignmentId) => {
-  return new Promise((resolve) => {
-    if (typeof newSolution.userSkills === 'object') {
+  return new Promise(resolve => {
+    if (typeof newSolution.userSkills === "object") {
       Promise.all([
         admin
           .database()
@@ -11,31 +11,30 @@ exports.handler = (newSolution, studentId, assignmentId) => {
         admin
           .database()
           .ref(`/problemCodingSkills/${assignmentId}`)
-          .once("value"),
+          .once("value")
       ]).then(data => {
         const pySkills = data[0].exists() ? data[0].val() : {};
         const problemSkills = data[1].exists() ? data[1].val() : {};
-
         // add new Skills
-        Object.keys((newSolution.userSkills || {})).forEach(key => {
+        Object.keys(newSolution.userSkills || {}).forEach(key => {
           Object.keys((newSolution.userSkills || {})[key]).forEach(subKey => {
             if (!pySkills[key]) {
-              pySkills[key] = {}
+              pySkills[key] = {};
             }
             if (!pySkills[key][subKey]) {
               pySkills[key][subKey] = {};
             }
-            pySkills[key][subKey][assignmentId] = true;
+            pySkills[key][subKey][assignmentId] = Date.now();
 
             if (!problemSkills[key]) {
-              problemSkills[key] = {}
+              problemSkills[key] = {};
             }
             if (!problemSkills[key][subKey]) {
               problemSkills[key][subKey] = {};
             }
-            problemSkills[key][subKey][studentId] = true;
-          })
-        })
+            problemSkills[key][subKey][studentId] = Date.now();
+          });
+        });
 
         // update user & problem python skill
         return Promise.all([
@@ -47,11 +46,10 @@ exports.handler = (newSolution, studentId, assignmentId) => {
             .database()
             .ref(`/problemCodingSkills/${assignmentId}`)
             .set(problemSkills)
-        ])
-      })
+        ]);
+      });
     } else {
       resolve();
     }
-  })
-
+  });
 };
