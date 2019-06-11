6ee66977d8ad120d6b72dceb72fd68ffc42eb4d0
Aishwarya Lakshminarasimhan <aishwaryarln@gmail.com>
2019-06-11 10:03:36 +0800

    Public path joined status update on load

--- src/containers/Paths/selector.js
+++ src/containers/Paths/selector.js
@@ -1,4 +1,5 @@
 import { createSelector } from "reselect";
+import isEmpty from "lodash/isEmpty";
 
 const getJoinedPath = state => state.paths.joinedPaths;
 const getPublicPath = state => state.firebase.data.publicPaths;
@@ -7,15 +8,21 @@ export const publicPathSelector = createSelector(
   getPublicPath,
   getJoinedPath,
   (publicPaths, joinedPaths) => {
-    //let counter = false;
-    Object.keys(publicPaths || {}).forEach(key => {
-      if (joinedPaths && joinedPaths[key]) {
-        publicPaths[key].solutions = joinedPaths[key].solutions;
-        publicPaths[key].totalActivities = joinedPaths[key].totalActivities;
-        //counter = true;
+    let counter = false;
+    if (joinedPaths && isEmpty(joinedPaths)) {
+      return publicPaths;
+    }
+    var modifiedPublicPaths = { ...publicPaths };
+    Object.keys(joinedPaths || {}).forEach(key => {
+      if (publicPaths && publicPaths[key]) {
+        modifiedPublicPaths[key].solutions = joinedPaths[key].solutions;
+        modifiedPublicPaths[key].totalActivities =
+          joinedPaths[key].totalActivities;
+        counter = true;
       }
     });
-    //FIXIT : fix progress to be rendered on initial load of paths screen
-    return publicPaths;
+    if (counter) {
+      return modifiedPublicPaths;
+    }
   }
 );
