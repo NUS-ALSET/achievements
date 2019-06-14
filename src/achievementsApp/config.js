73e8a064735a780f82d78d4d3ffa69d7cd9b210d
thangamani <thangamani.r@gmail.com>
2019-06-14 16:44:06 +0800

    Changed the config to include dev values

--- src/achievementsApp/config.js
+++ src/achievementsApp/config.js
@@ -34,12 +34,12 @@ export const APP_SETTING = {
 
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyDQuGo8wKcxLbLZo56aYWLTdP2qrbMamYQ",
-  authDomain: "achievements-prod.firebaseapp.com",
-  databaseURL: "https://achievements-prod.firebaseio.com",
-  projectId: "achievements-prod",
-  storageBucket: "achievements-prod.appspot.com",
-  messagingSenderId: "829016923358"
+  apiKey: "AIzaSyA232bBlzWT0fl3ST_KVC3Aay41yTMz5vM",
+  authDomain: "achievements-dev.firebaseapp.com",
+  databaseURL: "https://achievements-dev.firebaseio.com",
+  projectId: "achievements-dev",
+  storageBucket: "achievements-dev.appspot.com",
+  messagingSenderId: "479020625755"
 };
 firebase.initializeApp(config);
 export const firebaseConfig = firebase;
