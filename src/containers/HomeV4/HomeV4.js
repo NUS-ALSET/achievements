bab70ff6137e757fa137fad73e256d6e856197e5
SG <sgng1@e.ntu.edu.sg>
2019-07-04 09:27:51 +0100

    edits to homepage and now it only shows for unauthenticated users

--- src/containers/HomeV4/HomeV4.js
+++ src/containers/HomeV4/HomeV4.js
@@ -1,8 +1,9 @@
 import React from "react";
 import AppLogo from "../../assets/NUS_ALSET_Achievements_Logo.png";
 import Button from "@material-ui/core/Button";
+import styles from "./HomeV4.module.css";
 
-function HomeV4() {
+function HomePage(props) {
   return (
     <main>
       <section
@@ -17,6 +18,15 @@ function HomeV4() {
       >
         <img src={AppLogo} style={{ minWidth: "200px", maxWidth: "30%", mixBlendMode: "darken" }} alt="" />
         <p style={{ fontWeight: 200, fontSize: "1.5rem", textAlign: "center" }}>A learning platform for programmers</p>
+        <Button
+          color="primary"
+          size="large"
+          variant="contained"
+          style={{ marginTop: "1rem" }}
+          onClick={props.loginHandler}
+        >
+          Get started
+        </Button>
       </section>
       <section
         style={{
@@ -27,15 +37,7 @@ function HomeV4() {
           flexWrap: "wrap"
         }}
       >
-        <article
-          style={{
-            flexGrow: 1,
-            textAlign: "center",
-            margin: "0.5rem",
-            backgroundColor: "white",
-            boxShadow: "rgba(0, 0, 0, 0.14) 0px 0px 4px 0px, rgba(0, 0, 0, 0.28) 0px 4px 8px 0px"
-          }}
-        >
+        <article className={styles.topHighlightCards}>
           <h3>Master Python</h3>
           <ul style={{ textAlign: "left" }}>
             <li>Practice on Jupyter notebooks</li>
@@ -43,15 +45,7 @@ function HomeV4() {
             <li>Attempt Machine Learning problems</li>
           </ul>
         </article>
-        <article
-          style={{
-            flexGrow: 1,
-            textAlign: "center",
-            margin: "0.5rem",
-            backgroundColor: "white",
-            boxShadow: "rgba(0, 0, 0, 0.14) 0px 0px 4px 0px, rgba(0, 0, 0, 0.28) 0px 4px 8px 0px"
-          }}
-        >
+        <article className={styles.topHighlightCards}>
           <h3>Take charge of learning</h3>
           <ul style={{ textAlign: "left" }}>
             <li>Complete activities at your own pace</li>
@@ -59,15 +53,7 @@ function HomeV4() {
             <li>Download your data for analysis</li>
           </ul>
         </article>
-        <article
-          style={{
-            flexGrow: 1,
-            textAlign: "center",
-            margin: "0.5rem",
-            backgroundColor: "white",
-            boxShadow: "rgba(0, 0, 0, 0.14) 0px 0px 4px 0px, rgba(0, 0, 0, 0.28) 0px 4px 8px 0px"
-          }}
-        >
+        <article className={styles.topHighlightCards}>
           <h3>Learn by teaching</h3>
           <ul style={{ textAlign: "left" }}>
             <li>Create custom notebook problems</li>
@@ -76,81 +62,95 @@ function HomeV4() {
           </ul>
         </article>
       </section>
-      <hr style={{ marginTop: "2rem" }} />
-      <section>
-        <h2>Features</h2>
+      <section className={styles.textContent}>
+        <hr style={{ marginTop: "2rem" }} />
+        <h2>Highlights</h2>
         <article style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "center" }}>
-          <section
-            style={{
-              width: "90%",
-              minHeight: "200px",
-              backgroundSize: "cover",
-              backgroundImage: `url(https://res.cloudinary.com/dypcybha2/image/upload/q_auto:low,f_auto,o_30/v1562139090/jupyterActivity.png)`,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              backgroundColor: "white",
-              boxShadow: "rgba(0, 0, 0, 0.14) 0px 0px 4px 0px, rgba(0, 0, 0, 0.28) 0px 4px 8px 0px"
-            }}
-          >
-            <div style={{ padding: "1rem", alignSelf: "end", marginTop: "auto" }}>
-              <h3>Jupyter notebooks</h3>
-              <p>Our notebook problems support popular packages such as Pandas, Scikit-Learn and even tensorflow etc</p>
-            </div>
+          <section style={{ width: "100%" }}>
+            <div
+              className={styles.fullWidthCards}
+              style={{
+                backgroundImage: `url(https://res.cloudinary.com/dypcybha2/image/upload/q_auto:low,f_auto,o_80/v1562139090/jupyterActivity.png)`
+              }}
+            />
+            <h3>Jupyter notebooks</h3>
+            <p>Our notebook problems support popular packages such as Pandas, Scikit-Learn and even Tensorflow etc</p>
           </section>
-          <section
-            style={{
-              width: "90%",
-              minHeight: "200px",
-              backgroundSize: "cover",
-              backgroundImage: `url(https://res.cloudinary.com/dypcybha2/image/upload/q_auto:low,f_auto,o_30/v1562145128/myLearning.png)`,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              marginTop: "2rem",
-              backgroundColor: "white",
-              boxShadow: "rgba(0, 0, 0, 0.14) 0px 0px 4px 0px, rgba(0, 0, 0, 0.28) 0px 4px 8px 0px"
-            }}
-          >
-            <div style={{ padding: "1rem", alignSelf: "end", marginTop: "auto" }}>
-              <h3>Learning analytics</h3>
-              <p>Track your own learning analytics</p>
-            </div>
+          <section style={{ width: "100%" }}>
+            <div
+              className={styles.fullWidthCards}
+              style={{
+                backgroundImage: `url(https://res.cloudinary.com/dypcybha2/image/upload/q_auto:low,f_auto,o_80/v1562145128/myLearning.png)`
+              }}
+            />
+            <h3>Learning analytics</h3>
+            <p>Track your own learning analytics</p>
           </section>
-          <section
-            style={{
-              width: "90%",
-              minHeight: "200px",
-              backgroundSize: "cover",
-              backgroundImage: `url(https://res.cloudinary.com/dypcybha2/image/upload/q_auto:low,f_auto,o_30/v1562145432/customActivity.png)`,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              marginTop: "2rem",
-              backgroundColor: "white",
-              boxShadow: "rgba(0, 0, 0, 0.14) 0px 0px 4px 0px, rgba(0, 0, 0, 0.28) 0px 4px 8px 0px"
-            }}
-          >
-            <div style={{ padding: "1rem", alignSelf: "end", marginTop: "auto" }}>
-              <h3>Design your own custom problems</h3>
-              <p>Create your own custom notebook / serverless problems and more!</p>
-            </div>
+          <section style={{ width: "100%" }}>
+            <div
+              className={styles.fullWidthCards}
+              style={{
+                backgroundImage: `url(https://res.cloudinary.com/dypcybha2/image/upload/q_auto:low,f_auto,o_80/v1562145432/customActivity.png)`
+              }}
+            />
+            <h3>Design your own custom problems</h3>
+            <p>Create your own custom notebook / serverless problems and more!</p>
           </section>
         </article>
       </section>
-      <hr style={{ marginTop: "2rem" }} />
-      <section>
+      <section className={styles.textContent}>
+        <hr style={{ marginTop: "2rem" }} />
         <h2>Accessing Achievements</h2>
-        <p>All the features and course material in Achievements are free for anyone to access.</p>
-        <p>You just need to login with your Google account.</p>
-        <Button color="primary" size="large" variant="contained" style={{ marginTop: "1rem" }}>
+        <h3>Pricing</h3>
+        <p>All the features and course material in Achievements are free for everyone to access.</p>
+        <h3>Guide</h3>
+        <p>
+          We have an extensive catalogue of publicly accessible activities curated into{" "}
+          <i>
+            <b>Paths</b>
+          </i>{" "}
+          for you to explore.
+        </p>
+        <h3>Getting started</h3>
+        <p>Just login with your Google account.</p>
+        <Button
+          color="primary"
+          size="large"
+          variant="contained"
+          style={{ marginTop: "1rem" }}
+          onClick={props.loginHandler}
+        >
           Get started
+        </Button>
+      </section>
+      <section className={styles.textContent}>
+        <hr style={{ marginTop: "2rem" }} />
+        <h2>Achievements for educators</h2>
+        <h3>Paths and path activities</h3>
+        <p>Create your own paths and path activities to share with your students, friends and family.</p>
+        <p>
+          Choose from a wide range of options when creating your activities ie. from youtube / text to Jupyter notebooks
+          problems.
+        </p>
+        <h3>Cohorts and courses</h3>
+        <p>
+          Anyone can also create courses to enroll students, assign problems for them to complete and track their
+          progress.
+        </p>
+        <p>Use Cohorts to organise multiple courses.</p>
+        <h3>Getting started</h3>
+        <p>Login with your Google account to start educating on Achievements!</p>
+        <Button
+          color="primary"
+          size="large"
+          variant="outlined"
+          style={{ marginTop: "1rem" }}
+          onClick={props.loginHandler}
+        >
+          Get educating
         </Button>
       </section>
     </main>
   );
 }
-export default HomeV4;
+export default HomePage;
