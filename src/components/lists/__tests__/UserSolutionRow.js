7a3ed916d29796b7a6f30339711a21bc32878786
Lee Ying Yang <overaneout@gmail.com>
2019-08-13 22:20:30 +0800

    Improved unit test for PathList, UserSolutionRow

--- src/components/lists/__tests__/UserSolutionRow.js
+++ src/components/lists/__tests__/UserSolutionRow.js
@@ -1,16 +1,16 @@
 import React from "react";
 import sinon from "sinon";
-import { createShallow } from "@material-ui/core/test-utils";
+import { createShallow, createMount } from "@material-ui/core/test-utils";
 import { UserSolutionRow } from "../UserSolutionRow";
 
 describe("<UserSolutionRow />", () => {
   let shallow, mockDispatch;
   beforeEach(() => {
-    shallow = createShallow();
+    shallow = createMount();
     mockDispatch = sinon.spy();
   });
 
-  it("Should test getSolution function with without activity in props", () => {
+  it("Should test getSolution function without activity in props", () => {
     const props = {
       dispatch: mockDispatch,
       userId: "12345678910111213",
@@ -23,5 +23,23 @@ describe("<UserSolutionRow />", () => {
     const component = shallow(<UserSolutionRow {...props} />);
     const text_sol = component.instance().getSolution(props.solution);
     expect(text_sol).toEqual("Test text solution");
+  });
+  it("Should test getSolution function with activity in props", () => {
+    const props = {
+      dispatch: mockDispatch,
+      userId: "12345678910111213",
+      pathName: "Default",
+      status: false,
+      activity: { type: "jupyterInline" },
+      classes: {},
+      solution: {
+        solution: '{ "test": "Test text solution" }',
+        updatedAt: new Date()
+      }
+    };
+    //parse json
+    const component = shallow(<UserSolutionRow {...props} />);
+    const text_sol = component.instance().getSolution(props.solution);
+    expect(text_sol).toEqual({ test: "Test text solution" });
   });
 });
