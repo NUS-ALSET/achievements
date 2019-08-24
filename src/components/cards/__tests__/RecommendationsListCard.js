96f8df6da9da023a660696ca57c69136f70eb8c1
overaneout <overaneout@gmail.com>
2019-08-24 19:52:24 +0800

    Cleared up unused imports

--- src/components/cards/__tests__/RecommendationsListCard.js
+++ src/components/cards/__tests__/RecommendationsListCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { createShallow, createMount } from "@material-ui/core/test-utils";
+import { createShallow } from "@material-ui/core/test-utils";
 import sinon from "sinon";
 import { RecommendationsListCard } from "../RecommendationsListCard";
 import AdbIcon from "@material-ui/icons/Adb";
